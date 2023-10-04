b9f00b764bc0fec20cdb0ef3bf0651bedecd6917
dominikdeimel <info@dominikdeimel.com>
2023-10-04 19:21:17 +0200

    chore(setup): setup eslint and precommit hooks

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.vue'],
+  },
+  plugins: ['@typescript-eslint', 'vue'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    Hallo Welt
-  </div>
+  <div>Hallo Welt</div>
 </template>
